d18c1882286c9394c3c79d94a91e67624139111c
Wang Yueyi <yueyiwang@Yueyis-MacBook-Air.local>
2023-06-17 10:35:14 -0400

    FT and test added

--- perturb_real_NN.py
+++ perturb_real_NN.py
@@ -31,6 +31,19 @@ def gen_qindices(qlist):
     return qindices
 
 
+def kgrid_full(knum,a=1):
+    k1=np.linspace(-np.pi/a,np.pi/a,num=knum+1)
+    k2=np.roll(k1,1)
+    kave=(k1+k2)/2
+    klist=kave[-knum:] 
+    # print('klist=',klist)
+    k1, k2, k3 = np.meshgrid(klist, klist, klist, indexing='ij')
+    kx=0.5*(-k1+k2+k3)
+    ky=0.5*(k1-k2+k3)
+    kz=0.5*(k1+k2-k3)
+    return kx,ky,kz
+
+
 # Use this, we want to generate an array G_sk(iomega) as a function of iomega.
 # instead we prepare alpha and then generate Gf in the polarization
 def z(beta,mu,sig):
@@ -59,26 +72,27 @@ def ext_sig(beta,sig):
     return allsig
 
 def G_offdiag(knum,z_A,z_B,a=1):
-    kall=np.linspace(-np.pi/a,np.pi/a,num=knum+1)
-    kroll=np.roll(kall,1)
-    kave=(kall+kroll)/2
-    klist=kave[-knum:] 
     n=z_A.size
-    k1, k2, k3 = np.meshgrid(klist, klist, klist, indexing='ij')
-    kx=0.5*(-k1+k2+k3)
-    ky=0.5*(k1-k2+k3)
-    kz=0.5*(k1+k2-k3)
+    kx,ky,kz=kgrid_full(knum)
     G_offdiag=np.zeros((n,knum,knum,knum),dtype=np.complex128)
     zazb=z_A*z_B
     dis=dispersion(kx, ky, kz)
     G_offdiag = dis / (zazb[:, None, None, None] - dis**2)
     return G_offdiag
 
+def FT_3D(knum,G_k,Rx,Ry,Rz):# usually R is like (0,0,1)
+    n=np.shape(G_k)[0]
+    G_real=np.zeros(n,dtype=complex)
+    kx,ky,kz=kgrid_full(knum)
+    exp_factor=np.exp(-1j*(kx*Rx+ky*Ry+kz*Rz))
+    G_real=np.sum(G_k*exp_factor,axis=(1,2,3))/knum**3
+    return G_real
+
 
 #--------------test functions below---------
 
 def G_test(a=1):
-    start_time = time.time()
+    # start_time = time.time()
     U=2.0
     mu=U/2
     T=0.01
@@ -89,14 +103,13 @@ def G_test(a=1):
     z_A=z(beta,mu,sigA)
     z_B=z(beta,mu,sigB)
     n=sigA.size
-    allsigA=ext_sig(beta,sigA)
+    # allsigA=ext_sig(beta,sigA)
     # print('n=',n,)
-    knum=10
+    knum=20
     G_off=G_offdiag(knum,z_A,z_B)
     fermion_om = (2*np.arange(4*n)+1-4*n)*np.pi/beta
-    fermion_iom=1j*fermion_om
-    time_G=time.time()
-    print("time to calculate prepare 2 G is {:.6f} s".format(time_G-start_time))
+    # time_G=time.time()
+    # print("time to calculate prepare 2 G is {:.6f} s".format(time_G-start_time))
     kxind=3
     kyind=4
     kzind=5
@@ -104,6 +117,15 @@ def G_test(a=1):
     plt.plot(fermion_om,G_off[:,kxind,kyind,kzind].imag,label='Gk_off_imag')
     # plt.plot(fermion_om,G_off[:,knum-1-kxind,kyind,kzind].real,label='G-k_off_real')
     # plt.plot(fermion_om,G_off[:,knum-1-kxind,kyind,kzind].imag,label='G-k_off_imag')
+    plt.legend()
+    plt.show()
+    #-------------FT test--------------
+    Greal_100=FT_3D(knum,G_off,1,0,0)
+    Greal_000=FT_3D(knum,G_off,0,0,0)
+    plt.plot(fermion_om,Greal_100.real,label='Gr100_off_real')
+    plt.plot(fermion_om,Greal_100.imag,label='Gr100_off_imag')
+    plt.plot(fermion_om,Greal_000.real,label='Gr000_off_real')
+    plt.plot(fermion_om,Greal_000.imag,label='Gr000_off_imag')
     plt.legend()
     plt.show()
     return 0
